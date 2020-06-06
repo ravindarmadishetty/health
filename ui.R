d31ab4aac8305d70a333e9cca486c49d43c058b2
ravindarmadishetty <44523282+ravindarmadishetty@users.noreply.github.com>
2020-06-06 20:37:09 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,3 @@
-## Install R packages
-packages <- c('dplyr','leaflet','sp','tidyverse','readxl','shinythemes','shiny','dplyr','esquisse')
-install_if_missing <- function(p) {
-if (!p %in% rownames(installed.packages())) {
-install.packages(p)
-}
-}
-invisible(sapply(packages, install_if_missing))
-
 
 # example from http://shiny.rstudio.com/gallery/kmeans-example.html
 library(sp)
